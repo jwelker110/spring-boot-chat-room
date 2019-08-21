f81721698d6e5579beeb76d462ed2f0012146bb2
Jonathan Welker <jwelker110@users.noreply.github.com>
2019-08-21 09:36:01 -0500

    Remove completed todos

--- src/main/java/edu/udacity/java/nano/WebSocketChatApplication.java
+++ src/main/java/edu/udacity/java/nano/WebSocketChatApplication.java
@@ -31,7 +31,6 @@ public class WebSocketChatApplication {
      */
     @GetMapping("/index")
     public ModelAndView index(String username, HttpServletRequest request) throws UnknownHostException {
-        //TODO: add code for login to chatroom.
         if (username == null || username.trim().equals("")) {
             return new ModelAndView("/login");
         }
